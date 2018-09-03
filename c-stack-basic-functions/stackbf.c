ea076b159bd39d2cbe19076f1f9bee795fb2c3b3
Clauber Stipkovic <clauber.halic@gmail.com>
2018-09-03 20:51:01 -0300

    Just trailing whitespace

--- c-stack-basic-functions/stackbf.c
+++ c-stack-basic-functions/stackbf.c
@@ -1,41 +1,41 @@
 /*
-* STACK Basic Functions
-* stackbf.c
-*
-* Contributors: 
-*   Clauber Stipkovic <clauber.halic@gmail.com>
-*/
- 
+ * STACK Basic Functions
+ * stackbf.c
+ *
+ * Contributors:
+ *   Clauber Stipkovic <clauber.halic@gmail.com>
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "stackbf.h"
- 
+
 // Initialize Stack
 void initializeStackBF(_STACK_ *p, int MAXSTACK) {
 	p->topMax = MAXSTACK;
 	p->stack = (int *)malloc(sizeof(int) *MAXSTACK);
 	p->t = -1;
 }
- 
+
 // Verifie if the Stack is Empty
 int isEmptyStackBF(_STACK_ p) {
 	if(p.t == -1)
 		return 1;
 	return 0;
 }
- 
+
 // Verifies if the Stack is Full
 int isFullStackBF(_STACK_ p) {
 	if(sizeStackBF(p) == p.topMax)
 		return 1;
 	return 0;
 }
- 
+
 // Returns the size of Stack
 int sizeStackBF(_STACK_ p) {
 	return (p.t + 1);
 }
- 
+
 // Push a number at the Stack
 int pushStackBF(_STACK_ *p, int number) {
 	if(p->t + 1 < p->topMax) {
--- c-stack-basic-functions/stackbf.h
+++ c-stack-basic-functions/stackbf.h
@@ -1,22 +1,22 @@
 /*
-* STACK Basic Functions
-* stackbf.h
-*
-* Contributors: 
-*   Clauber Stipkovic <clauber.halic@gmail.com>
-*/
- 
-// TODO: At the main file, define the "MAXSTACK" variable to use the stack 
+ * STACK Basic Functions
+ * stackbf.h
+ *
+ * Contributors:
+ *   Clauber Stipkovic <clauber.halic@gmail.com>
+ */
+
+// TODO: At the main file, define the "MAXSTACK" variable to use the stack
 // basic functions
- 
+
 #ifndef STACKBF_H_
- 
+
 typedef struct stack{
 	int topMax;
 	int t;
 	int *stack;
 }_STACK_;
- 
+
 void initializeStackBF(_STACK_ *, int);
 int isEmptyStackBF(_STACK_);
 int isFullStackBF(_STACK_);
@@ -24,7 +24,7 @@ int sizeStackBF(_STACK_);
 int pushStackBF(_STACK_ *, int);
 int popStackBF(_STACK_ *);
 int getTopStackBF(_STACK_, int *);
- 
+
 #define STACKBF_H_
- 
+
 #endif /* STACKBF_H_ */
